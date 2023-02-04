ed55d62f7291c25a6bb9d5fcde48706e4bf59541
Fidel Soto <fidelsoto@gmail.com>
2023-02-04 03:43:22 -0400

    Added dialogue scripts, polished root animation

---
+++ Assets/Scripts/DialogueHelper.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueHelper : MonoBehaviour
+{
+	public Text label;
+	public Image frame;
+	public GameObject cursor;
+	public static DialogueHelper instance;
+	public bool active = false;
+	public bool forceEnd = false;
+	public bool permanent = false;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	private void Start()
+	{
+		frame.color = new Color32(0, 0, 0, 0);
+		label.text = "";
+	}
+
+	public IEnumerator ShowText(string content)
+	{
+		frame.color = new Color32(0, 0, 0, 233);
+		label.text = "";
+		for (int i = 0; i < content.Length; i++)
+		{
+			label.text += content[i];
+			yield return new WaitForSeconds(0.01f);
+			if (forceEnd)
+			{
+				label.text = content;
+				break;
+			}
+		}
+		yield return new WaitForSeconds(0.01f);
+		forceEnd = false;
+		if (active) cursor.SetActive(true);
+	}
+
+	public IEnumerator DisplayDialogue(string content/*, PlayerInteraction interaction*/)
+	{
+		active = true;
+		//interaction.enabled = false;
+		//interaction.GetComponent<PlayerMove>().enabled = false;
+		yield return StartCoroutine(ShowText(content));
+		yield return StartCoroutine(WaitToCloseDialogue());
+		//interaction.GetComponent<PlayerMove>().enabled = true;
+		//interaction.enabled = true;
+	}
+
+	public IEnumerator WaitToCloseDialogue()
+	{
+		while (cursor.activeInHierarchy)
+		{
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	private void Update()
+	{
+		if (permanent) return;
+		if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)))
+		{
+			if (cursor.activeInHierarchy)
+			{
+				EndDialogue();
+			}
+			else
+			{
+				forceEnd = true;
+			}
+		}
+	}
+
+	public void EndDialogue()
+	{
+		forceEnd = true;
+		cursor.SetActive(false);
+		active = false;
+		//GameManager.instance.StartCoroutine(GameManager.instance.PlaySound("Tap", 0f, 1f));
+		frame.color = new Color32(0, 0, 0, 0);
+		label.text = "";
+		active = false;
+	}
+}
---
+++ Assets/Scripts/SequencesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequencesController : MonoBehaviour
+{
+    Coroutine previous = null;
+    public IEnumerator DisplayText(string content)
+    {
+        if (previous != null)
+        {
+            StopCoroutine(previous);
+        }
+        //NPC.GetComponent<Animator>().Play("Talking");
+        DialogueHelper.instance.active = true;
+        previous = StartCoroutine(DialogueHelper.instance.ShowText(content));
+
+        /*int soundId = 4;
+        if (Random.Range(0, 2) == 1)
+        {
+            soundId = 3;
+        }
+        if (content.Contains("?"))
+        {
+            soundId = 1;
+            if (Random.Range(0, 2) == 1)
+                soundId = 2;
+        }
+        string talkingSound = "Talk" + soundId;
+        GameManager.instance.StartCoroutine(GameManager.instance.PlaySound(talkingSound, 0f, 1f));
+        LOLSDK.Instance.SpeakText(content);*/
+
+        while (DialogueHelper.instance.active)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+	private IEnumerator Start()
+	{
+        yield return null;
+        yield return StartCoroutine(DisplayText("Welcome to the world..."));
+    }
+}
